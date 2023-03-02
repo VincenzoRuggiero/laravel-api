a2376b9d46267a86cc96683d43e863f56250c83d
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-02 18:28:46 +0100

    Showing single project Api results

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,13 +8,20 @@ use App\Http\Controllers\Controller;
 
 class ProjectController extends Controller
 {
-    public function index(){
+    public function index() {
 
         $projects = Project::all();
 
         return response()->json([
             'success' => true,
             'results' => $projects
+        ]);
+    }
+
+    public function show(Project $project) {
+        return response()->json([
+            'success' => true,
+            'results' => $project
         ]);
     }
 }
