12391fa2426130fa05b0c6c06feba17d9801ca57
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-06 16:50:09 +0100

    Add Api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TechnologyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/projects', [ProjectController::class, 'index'])->name('api.projects.index');
+Route::get('/projects', [ ProjectController::class, 'index' ])->name('api.projects.index');
+Route::get('/projects/{project}', [ ProjectController::class, 'show' ])->name('api.projects.show');
+
+// Route::get('/types', [TypeController::class, 'index'])->name('api.types.index');
+// Route::get('/types/{type}', [ TypeController::class, 'show' ])->name('api.types.show');
+
+// Route::get('/technologies', [TechnologyController::class, 'index'])->name('api.technologies.index');
+// Route::get('/technologies/{technology}', [ TechnologyController::class, 'show' ])->name('api.technologies.show');
