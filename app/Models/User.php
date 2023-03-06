a6c6c77c8c784af6ca9b6171221f875365916a1f
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-06 16:59:09 +0100

    Model fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,6 @@ class User extends Authenticatable
     }
 
     public function roles(){
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(Role::class);
     }
 }
